f43eb49d3509eeba406d3203a0c6ad06042520f7
Veeresh B V <Veereshbv04@gmail.com>
2021-12-06 21:34:50 +0530

    random function object and function done

--- app.js
+++ app.js
@@ -4,12 +4,33 @@ const uppercaseInput = document.getElementById("uppercase");
 const lowercaseInput = document.getElementById("lowercase");
 const numbersInput = document.getElementById("numbers");
 const symbolsInput = document.getElementById("symbols");
+const resultDiv = document.getElementById("result");
+const clipboard = document.getElementById("clipboard");
+
+function getRandomLower(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber(){
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol(){
+    const symbols = "!@#$%^&*()_+-={}[];:,./<>?";
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+const randomFunction = {
+    lower: getRandomLower,
+    upper:getRandomUpper,
+    number:getRandomNumber,
+    Symbol:getRandomSymbol
+}
 
 
-symbolsInput.addEventListener("mouseover", ()=>{
-    console.log("g")
-})
 
-generateBtn.addEventListener("mouseover", ()=>{
-    console.log("de")
-})
+
