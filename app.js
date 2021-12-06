c5944ab2c386147d2ccd44ff0c365fdb238339a7
Veeresh B V <Veereshbv04@gmail.com>
2021-12-06 21:21:12 +0530

    all element wiring is working

---
+++ app.js
@@ -0,0 +1,15 @@
+const generateBtn = document.getElementById("generate");
+const lengthInput = document.getElementById("length");
+const uppercaseInput = document.getElementById("uppercase");
+const lowercaseInput = document.getElementById("lowercase");
+const numbersInput = document.getElementById("numbers");
+const symbolsInput = document.getElementById("symbols");
+
+
+symbolsInput.addEventListener("mouseover", ()=>{
+    console.log("g")
+})
+
+generateBtn.addEventListener("mouseover", ()=>{
+    console.log("de")
+})
